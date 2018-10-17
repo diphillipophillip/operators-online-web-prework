bbf14395f26a8b686d12fa6d3c687852ecd5147e
Phillip DiPhillipo <diphillipophillip@gmail.com>
2018-10-17 22:56:15 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,9 +11,9 @@ end
 
 
 def not_safe?(speed)
- speed > 60 ? true 
- speed < 40 ? true
- speed == 50 ? false
+ speed > 60 ? true : false 
+ speed < 40 ? true : false
+ speed == 50 ? false : true
 	
 end
 	
